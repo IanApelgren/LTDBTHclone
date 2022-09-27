fef3c958d697d1bdef1e56d08dd7703240a91f42
James <jcooke627@gmail.com>
2022-09-26 21:37:44 -0400

    sys tools pages now opens tools page. tools can return back to menu

--- src/com/ltdbth/gui/VirusTotal_1.java
+++ src/com/ltdbth/gui/VirusTotal_1.java
@@ -163,7 +163,8 @@ public class VirusTotal_1 extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        new SystemToolsPage().setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
